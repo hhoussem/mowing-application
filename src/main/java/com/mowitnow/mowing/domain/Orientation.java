5bd10ed7ea4ddde6f7c92db340c288781d339b8f
hhoussem <houssemeddine.hamrouni@gmail.com>
2023-02-09 20:49:23 +0100

    Refactor and improve Orientation.turn() method

--- src/main/java/com/mowitnow/mowing/domain/Orientation.java
+++ src/main/java/com/mowitnow/mowing/domain/Orientation.java
@@ -1,27 +1,41 @@
 package com.mowitnow.mowing.domain;
 
-import java.util.Map;
+import java.util.Arrays;
 
-import static com.mowitnow.mowing.domain.Instruction.*;
+import static com.mowitnow.mowing.domain.Instruction.LEFT;
+import static com.mowitnow.mowing.domain.Instruction.RIGHT;
 
 public enum Orientation {
-    NORTH('N'),
-    EAST('E'),
-    SOUTH('S'),
-    WEST('W');
+    NORTH('N', 0),
+    EAST('E', 90),
+    SOUTH('S', 180),
+    WEST('W', 270);
 
     private final char abbreviation;
+    private final int angle;
+    private static final int FULL_ANGLE = 360;
+    private static final int RIGHT_ANGLE = 90;
 
-    private final static Map<Orientation, Map<Instruction, Orientation>> INSTRUCTIONS_MAPPING = Map.of(NORTH, Map.of(LEFT, WEST, RIGHT, EAST),
-            EAST, Map.of(LEFT, NORTH, RIGHT, SOUTH),
-            SOUTH, Map.of(LEFT, EAST, RIGHT, WEST),
-            WEST, Map.of(LEFT, SOUTH, RIGHT, NORTH));
-
-    Orientation(char abbreviation) {
+    Orientation(char abbreviation, int angle) {
         this.abbreviation = abbreviation;
+        this.angle = angle;
     }
 
     Orientation turn(Instruction instruction) {
-        return FORWARD.equals(instruction) ? this : INSTRUCTIONS_MAPPING.get(this).get(instruction);
+        if (instruction.equals(RIGHT)) {
+            return getOrientationByAngle((this.angle + RIGHT_ANGLE) % FULL_ANGLE);
+        }
+        if (instruction.equals(LEFT)) {
+            return getOrientationByAngle((FULL_ANGLE + this.angle - RIGHT_ANGLE) % FULL_ANGLE);
+        }
+        return this;
+
     }
+
+    private Orientation getOrientationByAngle(int angle) {
+        return Arrays.stream(values()).filter(o -> o.angle == angle).findFirst().orElseThrow(
+                () -> new IllegalArgumentException("Can not determine orientation of the angle: " + angle));
+    }
+
+
 }

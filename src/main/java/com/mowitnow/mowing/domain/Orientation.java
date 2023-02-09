aa25dd218bfa1bf1fc654dcf42de28355ee24e0b
hhoussem <houssemeddine.hamrouni@gmail.com>
2023-02-09 14:03:55 +0100

    Implement orientation turn when receiving Left instruction

--- src/main/java/com/mowitnow/mowing/domain/Orientation.java
+++ src/main/java/com/mowitnow/mowing/domain/Orientation.java
@@ -1,6 +1,9 @@
 package com.mowitnow.mowing.domain;
 
+import java.util.Map;
+
 import static com.mowitnow.mowing.domain.Instruction.FORWARD;
+import static com.mowitnow.mowing.domain.Instruction.LEFT;
 
 public enum Orientation {
     NORTH('N'),
@@ -10,11 +13,16 @@ public enum Orientation {
 
     private final char abbreviation;
 
+    private final static Map<Orientation, Map<Instruction, Orientation>> INSTRUCTIONS_MAPPING = Map.of(NORTH, Map.of(LEFT, WEST),
+            EAST, Map.of(LEFT, NORTH),
+            SOUTH, Map.of(LEFT, EAST),
+            WEST, Map.of(LEFT, SOUTH));
+
     Orientation(char abbreviation) {
         this.abbreviation = abbreviation;
     }
 
     Orientation turn(Instruction instruction) {
-        return FORWARD.equals(instruction) ? this : null;
+        return FORWARD.equals(instruction) ? this : INSTRUCTIONS_MAPPING.get(this).get(instruction);
     }
 }
--- src/test/java/com/mowitnow/mowing/domain/OrientationTest.java
+++ src/test/java/com/mowitnow/mowing/domain/OrientationTest.java
@@ -3,6 +3,7 @@ package com.mowitnow.mowing.domain;
 import org.junit.jupiter.api.Test;
 
 import static com.mowitnow.mowing.domain.Instruction.FORWARD;
+import static com.mowitnow.mowing.domain.Instruction.LEFT;
 import static com.mowitnow.mowing.domain.Orientation.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,5 +15,13 @@ class OrientationTest {
         assertEquals(SOUTH, SOUTH.turn(FORWARD));
         assertEquals(EAST, EAST.turn(FORWARD));
         assertEquals(WEST, WEST.turn(FORWARD));
+    }
+
+    @Test
+    void orientation_turn_left() {
+        assertEquals(WEST, NORTH.turn(LEFT));
+        assertEquals(EAST, SOUTH.turn(LEFT));
+        assertEquals(NORTH, EAST.turn(LEFT));
+        assertEquals(SOUTH, WEST.turn(LEFT));
     }
 }
